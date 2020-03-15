49e68d4680ba1cfcc3e7253fcc5d08978232f432
sky <371005039@qq.com>
2020-03-15 21:50:11 +0800

    0,，1

--- New Unity Project/Assets/script/playerdata.cs
+++ New Unity Project/Assets/script/playerdata.cs
@@ -8,9 +8,7 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 using static playerdata;
 //用于处理角色数据相关
-public static playerinfo()
-{
-}
+
 public class playerdata : MonoBehaviour {
     public static int[] p_shuxing = new int[6];
     public static int[] p_wuxing = new int[5];
@@ -22,7 +20,89 @@ public class playerdata : MonoBehaviour {
     }
 
 // Update is called once per frame
-void Update() {
+    void Update() {
+
+    }
+    public struct shuxing
+    {
+        int STR;//力量
+        int DEX;//灵巧
+        int AGI; //敏捷
+        int VIT; //耐力
+        int INT; //智力
+        int CON; //体质
+        int WIS; //智慧
+        int PER;//感知
+        int PSY;//精神
+        int WIL;//意志HUIGEN
+        int MEILI;//魅力
+        int FUYUAN; //福缘
+        int HUIGEN; //慧根
+        int GENGU; //根骨
+    }
+    public struct skill_work
+    {
+        int gengzuo;//耕作
+        int chuyi;//厨艺
+        int tansuo;//探索
+        int shengcun;//生存
+        int fengren;//缝纫
+        int duanzao;//锻造
+        int mugong;//木工
+        int shigong;//石工
+        int yiyao;//医药
+        int jianzhu;//建筑
+        int yuanyi;//园艺
+
+    }
+    public struct skill_culture
+    {
+        int danqing;//丹青
+        int shishu;//诗书
+        int yinlv;//音律
+        int shushu;//术数
+        int yiqi;//弈棋
+        int daofa;//道法
+        int fofa;//佛学
+        int zaxue;//杂学
+        int chayi;//茶艺
+
+    }
+    public struct skill_fight
+    {
+        int jianfa;//剑法
+        int daofa;//刀法
+        int anqi;//暗器
+        int changbing;//长兵
+        int duanbing;//短兵
+        int roubo;//肉搏
+        int jianshu;//箭术
+        int shenfa;//身法
+        int qimen;//奇门
+        int qishu;//骑术
+        int qinggong;//轻功
+        int neigong;//内功
+
+    }
+    public struct roledata
+    {
+        float hp_cur;//当前生命
+        float ki_cur;//当前生命
+        float energy_cur;//当前生命
+        float hp_max;//最大生命
+        float ki_max;//最大生命
+        float energy_max;//最大生命
+        float hp_injured;//受伤生命
+        shuxing shuxing_cur;
+        shuxing shuxing_basis;
+        skill_work skill_work_cur;
+        skill_work skill_work_basis;
+        skill_culture skill_culture_cur;
+        skill_culture skill_culture_basis;
+        skill_fight skill_fight_cur;
+        skill_fight skill_fight_basis;
+
+
 
     }
     public static void Randshuxing()
