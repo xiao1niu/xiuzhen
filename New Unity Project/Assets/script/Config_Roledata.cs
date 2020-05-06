9215e7b186ff16c0b7384e95358cb5d96c02ad3d
371005039@qq.com <371005039@qq.com>
2020-05-06 09:53:38 +0800

    add

---
+++ New Unity Project/Assets/script/Config_Roledata.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Config_Roledata : MonoBehaviour
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public int roledata_id { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string roledata_type { get; set; }
+    /// <summary>
+    /// 敏捷
+    /// </summary>
+    public string roledata_name { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string inittype { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public int ishide { get; set; }
+
+    
+}
