e1df7ca50a57788412d9c8103ac52f9aea41f2b2
371005039@qq.com <371005039@qq.com>
2020-05-29 17:58:45 +0800

    新增游戏页面UI

---
+++ New Unity Project/Assets/page_gamehome.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class page_gamehome : MonoBehaviour
+{
+    public GameObject page_game;
+    public GameObject window_roleinfo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        page_action.Setactive(1, page_game);
+        page_action.Setactive(0, window_roleinfo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- New Unity Project/Assets/script/page_home.cs
+++ New Unity Project/Assets/script/page_home.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 public class page_home : MonoBehaviour {
     public GameObject home;
+    //public GameObject window_roleinfo;
     // Use this for initialization
     void Start()
     {
@@ -10,12 +11,14 @@ public class page_home : MonoBehaviour {
         Transform page_start = home.transform.Find("page_start");
         Transform page_load = home.transform.Find("loadplayer");
         Transform page_create = home.transform.Find("createplayer");
+        //page_action.Setactive(1, page_gamehome);
+        //page_action.Setactive(0, window_roleinfo);
         page_action.Setactive(1, page_start.gameObject);
-        page_action.Setactive(0, page_load.gameObject);
+       page_action.Setactive(0, page_load.gameObject);
         page_action.Setactive(0, page_create.gameObject);
     }
 
-        void Update() {
+    void Update() {
 
         }
     }
