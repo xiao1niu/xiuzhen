32c6ad182242349c3d7b090935b11f42390e2a56
371005039@qq.com <371005039@qq.com>
2020-06-22 17:53:59 +0800

    随机地图

--- New Unity Project/Assets/mapcreat.cs
+++ New Unity Project/Assets/mapcreat.cs
@@ -14,10 +14,14 @@ public class mapcreat : MonoBehaviour
     //大地图宽高
     public int levelW;
     public int levelH;
-    public int city = 1;
-    public int village = 3;
-    public int mine = 3;
-    public int stone = 5;
+    public float _maxHeight = 15;
+    public float _relief = 10f;
+
+    private float _seedX, _seedZ;
+    public int city = 10;
+    public int village = 30;
+    public int mine = 80;
+    public int stone = 100;
     // Start is called before the first frame update
     void Start()
     {
@@ -49,6 +53,9 @@ public class mapcreat : MonoBehaviour
 
     void InitMapTilesInfo()
     {
+        _seedX = Random.value * 100f;
+        _seedZ = Random.value * 100f;
+
         //初始化地图信息，即每个单位对应的地面类型
         TileType = new string[levelH * levelW];
         int check = 0;
@@ -56,72 +63,94 @@ public class mapcreat : MonoBehaviour
         int village_c = 0;
         int mine_c = 0;
         int stone_c = 0;
-        int dikuai;
+        int dikuai=100;
+        float height_y;
         for (int i = 0; i < levelH; i++)
         {
             for (int j = 0; j < levelW; j++)
             {
-                check = 0;
-                dikuai = Randmap();
+
+                float xSample = (i + _seedX) / _relief;
+                float zSample = (j + _seedZ) / _relief;
+                float noise = Mathf.PerlinNoise(xSample, zSample);
+                height_y = _maxHeight * noise;
+                if(height_y > _maxHeight * 0.89f)
+                { dikuai = 205; }
+                else if (height_y > _maxHeight * 0.85f)
+                { dikuai = 204; }
+                else if (height_y > _maxHeight * 0.80f)
+                { dikuai = 203; }
+                else if (height_y > _maxHeight * 0.74f)
+                { dikuai = 202; }
+                else if (height_y > _maxHeight * 0.65f)
+                { dikuai = 101; }
+                else if (height_y > _maxHeight * 0.20f)
+                {
+                    check = 0;
+                    dikuai = Randmap();
                     //Random.Range(1, TilesName.Count);
 
-                while (check == 0)
-                {
-                    switch (dikuai)
+                    while (check == 0)
                     {
-                        case 102://石山
-                            if (stone_c < stone)
-                            {
+                        switch (dikuai)
+                        {
+                            case 102://石山
+                                if (stone_c < stone)
+                                {
+                                    check = 1;
+                                    stone_c++;
+                                }
+                                else
+                                {
+                                    check = 0;
+                                }
+                                break;
+                            case 103://矿山
+                                if (mine_c < mine)
+                                {
+                                    check = 1;
+                                    mine_c++;
+                                }
+                                else
+                                {
+                                    check = 0;
+                                }
+                                break;
+                            case 2101://村庄
+                                if (village_c < village)
+                                {
+                                    check = 1;
+                                    village_c++;
+                                }
+                                else
+                                {
+                                    check = 0;
+                                }
+                                break;
+                            case 2102://城市
+                                if (city_c < city)
+                                {
+                                    check = 1;
+                                    city_c++;
+                                }
+                                else
+                                {
+                                    check = 0;
+                                }
+                                break;
+                            default:
                                 check = 1;
-                                stone_c++;
-                            }
-                            else
-                            {
-                                check = 0;
-                            }
-                            break;
-                        case 103://矿山
-                            if (mine_c < mine)
-                            {
-                                check = 1;
-                                mine_c++;
-                            }
-                            else
-                            {
-                                check = 0;
-                            }
-                            break;
-                        case 2101://村庄
-                            if (village_c < village)
-                            {
-                                check = 1;
-                                village_c++;
-                            }
-                            else
-                            {
-                                check = 0;
-                            }
-                            break;
-                        case 2102://城市
-                            if (city_c < city)
-                            {
-                                check = 1;
-                                city_c++;
-                            }
-                            else
-                            {
-                                check = 0;
-                            }
-                            break;
-                        default:
-                            check = 1;
-                            break;
+                                break;
+                        }
+                        if (check == 0)
+                        {
+                            dikuai = Randmap();
+                        }
                     }
-                    if (check == 0)
-                    {
-                        dikuai = Randmap();
-                    }
+
                 }
+                else if (height_y <= _maxHeight * 0.2f)
+                { dikuai = 101; }
                 TileType[i * levelW + j] = dikuai.ToString();
                 Debug.Log("dikuai:" + dikuai);
             }
@@ -170,11 +199,15 @@ public class mapcreat : MonoBehaviour
         Dictionary<int,int> weightlist=new Dictionary<int, int>();
         foreach (var info in Configinit.Config_Map)
         {
-            maxweight = maxweight + info.Value.weight;
-            //Debug.Log(info.Key + " " + info.Value.pic);
-            weightlist.Add( maxweight, info.Value.id);
+            if (info.Value.type != 2 & info.Value.type != 10) {
+                
+                maxweight = maxweight + info.Value.weight;
+                //Debug.Log(info.Key + " " + info.Value.pic);
+                weightlist.Add(maxweight, info.Value.id);
+            
+            }
         }
-        int weight=Random.Range(1, maxweight);
+        int weight=Random.Range(1, maxweight+1);
         int mat = maxweight;
         foreach (var info in weightlist)
         {
