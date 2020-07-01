9be5b9e6d2ea75ac86ca9d36d4659c88bdb74d67
371005039@qq.com <371005039@qq.com>
2020-07-01 18:22:58 +0800

    生成世界生成选项列表

--- New Unity Project/Assets/script/page_createworld.cs
+++ New Unity Project/Assets/script/page_createworld.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,7 +12,13 @@ public class page_createworld : MonoBehaviour
     {
 
     }
-
+    void Awake()
+    {
+        addtoggle("set_worldsize", 1);
+        addtoggle("set_resamount", 2);
+        addtoggle("set_forestamount", 3);
+        Debug.Log("-----settoggle");
+    }
 
     // Update is called once per frame
     void Update()
@@ -20,31 +27,52 @@ public class page_createworld : MonoBehaviour
     }
     void addtoggle (string title,int n)
     {
-        GameObject table_worldset = Instantiate(prefab_unit_worldset);
+        GameObject unit_worldset = Instantiate(prefab_unit_worldset);
         //Instantiate(prefab_savetip) as GameObject;
-        table_worldset.transform.SetParent(table_worldset.transform);
-        table_worldset.transform.localScale = new Vector3(1, 1, 1);
-        table_worldset.transform.name = "set_"+n;
-        table_worldset.transform.Find("unitname").GetComponent<Text>().text= title;
-        GameObject unit_toggle = table_worldset.transform.Find("table_toggle/Toggle0").gameObject;
-        string[] togglelist = setTogglelist("世界尺寸");
-        foreach (var info in togglelist)
+        unit_worldset.transform.SetParent(table_worldset.transform);
+        unit_worldset.transform.localScale = new Vector3(1, 1, 1);
+        unit_worldset.transform.name = "set_"+n;
+        string[] togglelist = setTogglelist(title);
+        unit_worldset.transform.Find("unitname").GetComponent<Text>().text= togglelist[0];
+        GameObject unit_toggle = unit_worldset.transform.Find("table_toggle/Toggle1").gameObject;
+        GameObject ToggleX=new GameObject();
+        //string[] togglelist = setTogglelist(title);
+        Debug.Log("{---" + togglelist.Length);
+        for (int num = 1; num < togglelist.Length; num++)
             {
-            
+            try
+            {
+                ToggleX = unit_toggle.transform.Find("Toggle" + num).gameObject;
+                
+            } 
+            catch(NullReferenceException e)
+            {
+                //Debug.Log("{0} First exception."+ e.Message);
+                ToggleX = Instantiate(unit_toggle);
+                ToggleX.transform.SetParent(table_worldset.transform.Find("table_toggle"));
+                ToggleX.transform.name = "Toggle" + num;
+                
+            }
+            finally
+            {
+                Debug.Log("Toggle" + num + "/Label");
+                //unit_toggle.GetComponent<Text>().text = togglelist[num];
+                ToggleX.transform.Find("Label").gameObject.GetComponent<Text>().text = togglelist[num];
+            }
         }
     }
     string[] setTogglelist(string title)
     {
         string[] toggle= { "1"};
         switch (title) { 
-            case"世界尺寸":
-                toggle = new string[] {"小","中", "大" };
+            case"set_worldsize":
+                toggle = new string[] { "世界尺寸","小","中", "大" };
                 break;
-            case "资源数量":
-                toggle = new string[] { "少量", "一般", "大量" };
+            case "set_resamount":
+                toggle = new string[] { "资源数量","少量", "一般", "大量" };
                 break;
-            case "森林范围":
-                toggle = new string[] { "少量", "一般", "大量" };
+            case "set_forestamount":
+                toggle = new string[] { "森林范围", "少量", "一般", "大量" };
                 break;
         }
         return toggle;
